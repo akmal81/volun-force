2ddba2c71bc7aabf91de013621bb3b784548407c
akmal81 <sah64078@gmail.com>
2024-12-23 20:25:25 +0600

    Add volunteer needed post added

--- src/pages/AddVolunteerPost/AddVolunteerPost.jsx
+++ src/pages/AddVolunteerPost/AddVolunteerPost.jsx
@@ -1,137 +1,178 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import useAuth from '../../hooks/useAuth';
+import { format } from 'date-fns';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import toast from 'react-hot-toast';
 
 const AddVolunteerPost = () => {
-   
+  const { user } = useAuth();
+  const [postDeadline, setPostDeadline] = useState(new Date());
+  const formatedDate = format(new Date(postDeadline), 'dd/MM/yyyy');
+  
+  const handleAddPost = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const thumbnail = form.thumbnail.value;
+    const postTitle = form.postTitle.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const location = form.location.value;
+    const volunteersNeeded = form.volunteersNeeded.value;
+    const deadline = postDeadline;
+    const organizerName = user?.displayName;
+    const organizerEmail = user?.email;
 
-    return (
-        <form className="card-body">
+    const addPostData = { thumbnail, postTitle, description, category, location, volunteersNeeded, deadline, organizerName, organizerEmail }
+    
 
-        {/* thumbnail */}
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Thumbnail</span>
-        </label>
-        <input type="url"
-         placeholder="" 
-         defaultValue={thumbnail}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
-      {/* post Title */}
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Post Title</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={postTitle}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
+    try {
+      await axios.post(`${import.meta.env.VITE_api_url}/posts`, addPostData
+      )
+      form.reset();
+      Swal.fire('Volunteer Need Post added Successfully!!!')
 
-      {/* description */}
-      
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Description</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={description}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
-      
-      {/* category */}
-      
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Category</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={category}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
-      
-      {/* Location */}
+    } catch (err) {
+      toast.error(err.message)
+    }
+  }
 
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Location</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={location}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
-      
-      {/* No of volunteers needed */}
+  return (
+    <div>
+      <h2>AddVolunteerPost</h2>
+      <div className=' space-y-4'>
+        <form onSubmit={handleAddPost} className="card-body">
 
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">No of volunteers needed</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={volunteersNeeded}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
+          {/* thumbnail */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Thumbnail</span>
+            </label>
+            <input type="url"
+              name='thumbnail'
+              placeholder="Photo url"
 
-      {/* Deadline */}
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Deadline</span>
-        </label>
-        <input type="date"
-         placeholder="" 
-         defaultValue={deadline}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
 
-      {/* Organizer name */}
+              className="input input-bordered"
+              required />
+          </div>
+          {/* post Title */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Post Title</span>
+            </label>
+            <input type="text"
+              name='postTitle'
+              placeholder="Post Title"
+              className="input input-bordered"
+              required />
+          </div>
 
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Organizer name</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={organizerName}
-         readOnly
-         className="input input-bordered" 
-         required />
-      </div>
+          {/* description */}
 
-        {/* Organizer email*/}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Description</span>
+            </label>
+            <input type="text"
+              name='description'
+              placeholder="Description"
 
-        <div className="form-control">
-        <label className="label">
-          <span className="label-text">Organizer email</span>
-        </label>
-        <input type="text"
-         placeholder="" 
-         defaultValue={organizerEmail}
-         readOnly
-         className="input input-bordered" 
-         required />
+
+              className="input input-bordered"
+              required />
+          </div>
+
+          {/* category */}
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <input type="text"
+              name='category'
+              placeholder="Category"
+              className="input input-bordered"
+              required />
+          </div>
+
+          {/* Location */}
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Location</span>
+            </label>
+            <input type="text"
+              name='location'
+              placeholder="Location"
+              className="input input-bordered"
+              required />
+          </div>
+
+          {/* No of volunteers needed */}
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">No of volunteers needed</span>
+            </label>
+            <input type="text"
+              name='volunteersNeeded'
+              placeholder="No of volunteers needed"
+              className="input input-bordered"
+              required />
+          </div>
+
+          {/* Deadline */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Deadline</span>
+            </label>
+            <DatePicker
+              name='deadline'
+              value={formatedDate}
+              onChange={date => setPostDeadline(date)}
+              dateFormat="dd/MM/yyyy"
+              className="input input-bordered"
+            />
+          </div>
+
+          {/* Organizer name */}
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Organizer name</span>
+            </label>
+            <input type="text"
+              name='organizerName'
+              placeholder=""
+              defaultValue={user.displayName}
+              readOnly
+              className="input input-bordered"
+              required />
+          </div>
+
+          {/* Organizer email*/}
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Organizer email</span>
+            </label>
+            <input type="text"
+              name='organizerEmail'
+              placeholder=""
+              defaultValue={user.email}
+              readOnly
+              className="input input-bordered"
+              required />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn bg-primary text-white">Add Post</button>
+          </div>
+        </form>
       </div>
-      
-      <div className="form-control mt-6">
-        <button className="btn bg-primary text-white text-lg">Request</button>
-      </div>
-    </form>
-    );
+    </div>
+  );
 };
 
 export default AddVolunteerPost;
